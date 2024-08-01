676a7a4608b1b2c6f1fa1a29ab5d68d34932ec2d
Chirag-bambhaniya <161549663+Chirag-bambhaniya@users.noreply.github.com>
2024-08-01 12:28:07 +0530

    Add files via upload

---
+++ portfolio2.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+  $('#progress1').animate({
+
+      width: '100%'
+
+  }, 2500);
+
+  $('#progress2').animate({
+
+      width: '90%'
+
+  }, 2500);
+
+  $('#progress3').animate({
+
+      width: '95%'
+
+  }, 2500);
+
+  $('#progress4').animate({
+
+      width: '80%'
+
+  }, 2500);
+
+  $('#progress5').animate({
+
+      width: '70%'
+
+  }, 2500);
+
+  $('#progress6').animate({
+
+      width: '80%'
+
+  }, 2500);
+
+  $('#progress7').animate({
+
+      width: '80%'
+
+  }, 2500);
+
+  $('#progress8').animate({
+
+      width: '80%'
+
+  }, 2500);
+
+});
